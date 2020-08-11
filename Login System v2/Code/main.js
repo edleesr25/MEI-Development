33760b9c79c8dd37cecf7d50edec68acd1a8751e
Edward Lee <edleesr25@herricksk12.org>
2020-08-11 11:12:02 -0400

    Added Signup Part, removed dark mode

--- Login System v2/Code/main.js
+++ Login System v2/Code/main.js
@@ -1,35 +1,6 @@
-// Color Theme
-if (document.getElementsByClassName("color-mode")[0].innerText = "Dark Mode") {
-    document.getElementsByClassName("color-mode")[0].addEventListener('click', function () {
-        document.getElementsByClassName("form-wrapper")[0].style.backgroundColor = "#1e1e1e";
-        document.getElementsByClassName("image-set")[0].style.backgroundColor = "#1e1e1e";
-        document.querySelector("h1").style.color = "#fff";
-        document.querySelector("#input-username").style.color = "fff";
-        document.querySelector("#input-password").style.color = "fff";
-        document.querySelectorAll("p")[0].style.color = "#fff";
-        document.querySelectorAll("p")[1].style.color = "#fff";
-        document.querySelectorAll("label")[0].style.color = "#fff";
-        document.querySelectorAll("label")[1].style.color = "#fff";
-        document.querySelectorAll("label")[2].style.color = "#fff";
-        document.querySelectorAll("label")[0].style.display = "none";
-        document.querySelectorAll("label")[1].style.display = "none";
-        document.querySelectorAll("label")[2].style.display = "none";
-        document.querySelectorAll("label")[0].style.display = "inline";
-        document.querySelectorAll("label")[1].style.display = "inline";
-        document.querySelectorAll("label")[2].style.display = "inline";
-        document.getElementsByClassName("color-mode")[0].innerText = "Light Mode";
-    });
-}
 // Image URL
 document.getElementsByClassName("submit")[0].addEventListener('click', function () {
     var image = document.getElementById("image-value").value;
     document.body.style.backgroundImage = "url('" + image + "')";
 });
-// No Username 
-document.getElementById("login-btn").addEventListener('click', function () {
-    // Check if there is a username
-    if (document.getElementById("input-username").value == null) {
-        document.getElementById("input-username").setAttribute("placeholder", "Must Include Username");
-        document.querySelector("#input-username::placeholder").style.color = "red";
-    }
-});
+
